5345dfcda12f908a4dfd926037e2e571627e4686
Adam Ayres <adam.ayres@lithium.com>
2025-05-14 15:00:42 -0700

    Search PoC

---
+++ public/script-loading.js
@@ -0,0 +1,156 @@
+const searchElementSelector = '[data-testid="SpotlightSearchIcon.Icon"]';
+const searchElementWithEventSelector = '[data-grazittisearch="true"]';
+const compiledReactAppScriptUrl = 'https://rawcdn.githack.com/adamayres/grazitti/eb738f0405021bce69f64ab959ec1cc1414352a1/dist/assets/index-BDQj9t_8.js';
+const grazittiAppRootId = 'gz-app-root';
+const searchOpenEventName = 'gz-search-open';
+const onAppLoadedEvents = [];
+let appLoaded = false;
+
+/**
+ * Adds a script link to the page
+ *
+ * @param scriptUrl the script URL
+ * @param id a unique identifier
+ * @return {Promise<unknown>} when the script loads
+ */
+async function addScriptLink(scriptUrl, id) {
+  if (!document.getElementById(id)) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.setAttribute('crossorigin', 'true');
+      script.setAttribute('id', id);
+      script.setAttribute('src', scriptUrl);
+      script.onload = () => resolve();
+      script.onerror = () => reject();
+      document.head.append(script);
+    });
+  }
+  return null;
+}
+
+/**
+ * Simple debounce function
+ *
+ * @param callback the function to debounce
+ * @param wait how long to debounce
+ * @return {(function(...[*]): void)|*} a callback to the original function
+ */
+function debounce(callback, wait) {
+  let timeoutId = null;
+  return (...args) => {
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+}
+
+/**
+ * Creates the custom react app root element
+ */
+function getOrCreateCustomReactAppRoot() {
+  const appRoot = document.getElementById(grazittiAppRootId);
+  if (appRoot) {
+    return appRoot;
+  }
+
+  const newAppRoot = document.createElement('div');
+  newAppRoot.setAttribute('id', grazittiAppRootId);
+  document.body.append(newAppRoot);
+  return newAppRoot;
+}
+
+/**
+ * Click handler for the Aurora Search Icon.
+ *
+ * @param event the click event
+ * @return {Promise<void>}
+ */
+async function clickHandler(event) {
+  event.preventDefault();
+  event.stopImmediatePropagation();
+
+  // Check if the custom react app root element exists, if not create it.
+  getOrCreateCustomReactAppRoot();
+
+  // Add the custom react search component
+  await addScriptLink(compiledReactAppScriptUrl, 'gz-app');
+
+  // Dispatch a custom event that can be used by the custom react component to know when to open
+  function callback() {
+    event.target.dispatchEvent(new Event(searchOpenEventName, { bubbles: true }));
+  }
+  if (appLoaded) {
+    callback();
+  } else {
+    onAppLoadedEvents.push(callback);
+  }
+}
+
+/**
+ * Adds a click handler to the Aurora search Icon
+ *
+ * @param searchElement the search icon element
+ */
+function addSearchIconClickHandler(searchElement = document.querySelector(searchElementSelector)) {
+  searchElement.addEventListener('click', clickHandler);
+  searchElement.dataset.grazittisearch = 'true';
+}
+
+// Debounced version
+const debouncedAddSearchIconClickHandler = debounce((searchElement) => addSearchIconClickHandler(searchElement), 500);
+
+
+/**
+ * Applies a mutation observer to the page to watch if the search icon is added or removed so
+ * we can add and remove the click handler.
+ */
+function applyPageObserver() {
+  const observerOptions = {
+    childList: true,
+    subtree: true
+  };
+
+  const observer = new MutationObserver(function(mutationsList) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        mutation.removedNodes.forEach(removedNode => {
+          if ('matches' in removedNode && removedNode.matches(searchElementWithEventSelector)) {
+            removedNode.removeEventListener('click', clickHandler);
+          }
+        });
+        mutation.addedNodes.forEach(addedNode => {
+          if ('matches' in addedNode && addedNode.matches(searchElementSelector) && !addedNode.matches(searchElementWithEventSelector)) {
+            debouncedAddSearchIconClickHandler(addedNode);
+          }
+        });
+      }
+    }
+  });
+
+  observer.observe(document.body, observerOptions);
+}
+
+function applyCustomAppRootLoadedObserver() {
+  const observerOptions = {
+    attributes: true,
+    attributesFilter: ['data-loaded']
+  };
+
+  const rootElement = getOrCreateCustomReactAppRoot();
+  const observer = new MutationObserver(function(mutationsList) {
+    if (mutationsList[0].target.dataset.loaded === 'true') {
+      appLoaded = true;
+      while (onAppLoadedEvents.length > 0) {
+        onAppLoadedEvents.pop()();
+      }
+    }
+  });
+
+  observer.observe(rootElement, observerOptions);
+}
+
+addSearchIconClickHandler();
+applyPageObserver();
+getOrCreateCustomReactAppRoot();
+applyCustomAppRootLoadedObserver();
---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import {useEffect, useState} from "react";
+
+function App() {
+  const [show, setShow] = useState(false);
+
+  function customOpenEventListener() {
+    setShow(true);
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('gz-search-open', customOpenEventListener);
+    document.getElementById('gz-app-root').dataset.loaded = 'true';
+
+    return () => {
+      document.body.removeEventListener('gz-search-open', customOpenEventListener);
+      document.getElementById('gz-app-root').dataset.loaded = null;
+    }
+  });
+
+  if (show) {
+    return <>
+      <div className="backdrop" style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: 1040,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'var(--lia-bs-modal-backdrop-bg)',
+        opacity: 'var(--lia-bs-modal-backdrop-opacity)'
+      }}></div>
+      <div role="dialog" onClick={() => setShow(false)} style={{position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: 1050,
+        display: 'block',
+        width: '100%',
+        height: '100%',
+        outline: 0,
+        overflowX: 'hidden',
+        overflowY: 'auto'
+      }}>
+        <div className="modal" style={{
+          margin: '1.5625rem auto',
+          marginTop: '56px',
+          position: 'relative',
+          width: 'auto',
+          maxWidth: 'min(53.75rem, calc(100vw - var(--lia-bs-grid-gutter-width)))'
+        }}>
+          <form onClick={(event) => {
+            event.stopPropagation()
+          }}>
+            <input placeholder="Custom search" style={{
+              width: '100%',
+              padding: '20px',
+              border: 0,
+              borderRadius: 'var(--lia-bs-input-border-radius-lg)'
+            }} />
+          </form>
+        </div>
+      </div>
+    </>
+  }
+}
+
+export default App
